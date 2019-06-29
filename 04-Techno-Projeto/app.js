9694374259a4a7591f47557ecb5108c50e45786c
José Malcher Jr <contato@josemalcher.net>
2019-06-28 23:29:18 -0300

    + fetch em produtos

--- 04-Techno-Projeto/app.js
+++ 04-Techno-Projeto/app.js
@@ -1,7 +1,13 @@
 const vm = new Vue({
     el:"#app",
     data: {
-        produtos: []
+        produtos: [],
+        produto: false,
+    },
+    filters:{
+        numeroPreco(valor){
+            return valor.toLocaleString("en-BR", {style: "currency", currency: "BRL"});
+        }
     },
     methods: {
         fetchProdutos(){
@@ -10,6 +16,13 @@ const vm = new Vue({
             .then(r => {
                 this.produtos = r;
             })
+        },
+        fetchProduto(id){
+            fetch(`./api/produtos/${id}/dados.json`)
+                .then( r => r.json())
+                .then(r => {
+                    this.produto = r;
+                })
         }
     },
     created() {
