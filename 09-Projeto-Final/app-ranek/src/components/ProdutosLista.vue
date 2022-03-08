8b6c4c3ada519065acfd7b419ebba0d9949ff9ea
José Malcher Jr <contato@josemalcher.net>
2022-03-08 10:38:35 -0300

    0904 Produtos

--- 09-Projeto-Final/app-ranek/src/components/ProdutosLista.vue
+++ 09-Projeto-Final/app-ranek/src/components/ProdutosLista.vue
@@ -1,15 +1,35 @@
 <template>
-<div>
-    <h2>lista</h2>
-</div>
+  <section class="produtos-container">
+    <div v-for="produto in produtos" :key="produto.id">
+      <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
+      <p class="preco">{{produto.preco}}</p>
+      <h2 class="titulo">{{produto.nome}}</h2>
+      <p>{{produto.descricao}}</p>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  name: 'ProdutosLista'
+  data () {
+    return {
+      produtos: null
+    }
+  },
+  methods: {
+    getProdutos () {
+      fetch('http://localhost:3000/produto')
+        .then(response => response.json())
+        .then(response => {
+          this.produtos = response
+        })
+    }
+  },
+  created () {
+    this.getProdutos()
+  }
 }
 </script>
 
-<style scoped>
-
+<style>
 </style>
