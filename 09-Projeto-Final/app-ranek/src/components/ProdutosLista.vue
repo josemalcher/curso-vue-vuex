dfd0df0bd0a516b00235673873b294bdea058ecc
José Malcher Jr <contato@josemalcher.net>
2022-03-08 15:11:51 -0300

    0905 Axios

--- 09-Projeto-Final/app-ranek/src/components/ProdutosLista.vue
+++ 09-Projeto-Final/app-ranek/src/components/ProdutosLista.vue
@@ -10,6 +10,8 @@
 </template>
 
 <script>
+import { api } from '@/services.js'
+
 export default {
   data () {
     return {
@@ -18,11 +20,16 @@ export default {
   },
   methods: {
     getProdutos () {
-      fetch('http://localhost:3000/produto')
-        .then(response => response.json())
+      api.get('/produto')
         .then(response => {
-          this.produtos = response
+          this.produtos = response.data
         })
+
+      // fetch('http://localhost:3000/produto')
+      //   .then(response => response.json())
+      //   .then(response => {
+      //     this.produtos = response
+      //   })
     }
   },
   created () {
