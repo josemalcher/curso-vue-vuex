157485bb65dcfc507ea6c85203bf1dc79ca41fa2
José Malcher Jr <contato@josemalcher.net>
2022-03-09 15:02:03 -0300

    0908 Input Estilo

--- 09-Projeto-Final/app-ranek/src/App.vue
+++ 09-Projeto-Final/app-ranek/src/App.vue
@@ -84,4 +84,25 @@ img {
 #main {
   flex: 1;
 }
+
+input,
+textarea {
+  border-radius: 4px;
+  border: 1px solid white;
+  padding: 15px;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  transition: all 0.3s;
+  font-size: 1rem;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  margin-bottom: 15px;
+}
+
+input:hover,
+input:focus,
+textarea:hover,
+textarea:focus {
+  outline: none;
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+  border-color: #87f;
+}
 </style>
--- 09-Projeto-Final/app-ranek/src/components/ProdutosBuscar.vue
+++ 09-Projeto-Final/app-ranek/src/components/ProdutosBuscar.vue
@@ -1,6 +1,6 @@
 <template>
   <form>
-    <input name="busca" id="busca" type="text" v-model="busca">
+    <input name="busca" id="busca" type="text" v-model="busca" placeholder="Buscar...">
     <input type="submit" id="lupa" value="Buscar" @click.prevent="buscarProdutos">
   </form>
 </template>
@@ -20,5 +20,34 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+form {
+  max-width: 600px;
+  margin: 30px auto 60px auto;
+  position: relative;
+}
+
+#busca {
+  width: 100%;
+  padding: 20px;
+  border: none;
+}
+
+#busca:focus,
+#busca:hover {
+  transform: scale(1.1);
+}
+
+#lupa {
+  width: 62px;
+  height: 62px;
+  background: url("../assets/search.svg") no-repeat center center;
+  text-indent: -150px;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  box-shadow: none;
+}
 </style>
