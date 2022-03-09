0700c79c1cfa6b9e584dc9cec3ea409b6ffcfdf5
José Malcher Jr <contato@josemalcher.net>
2022-03-09 14:45:15 -0300

    0907 Produtos Estilo

--- 09-Projeto-Final/app-ranek/src/components/ProdutosLista.vue
+++ 09-Projeto-Final/app-ranek/src/components/ProdutosLista.vue
@@ -1,10 +1,17 @@
 <template>
   <section class="produtos-container">
-    <div v-for="produto in produtos" :key="produto.id">
-      <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
-      <p class="preco">{{produto.preco}}</p>
-      <h2 class="titulo">{{produto.nome}}</h2>
-      <p>{{produto.descricao}}</p>
+    <div v-if="produtos && produtos.length" class="produtos">
+      <div class="produto" v-for="produto in produtos" :key="produto.id">
+        <router-link to="/">
+          <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
+          <p class="preco">{{produto.preco}}</p>
+          <h2 class="titulo">{{produto.nome}}</h2>
+          <p>{{produto.descricao}}</p>
+        </router-link>
+      </div>
+    </div>
+    <div v-else-if="produtos && produtos.length === 0">
+      <p class="sem-resultados">Busca sem resultados. Tente buscar outro termo.</p>
     </div>
   </section>
 </template>
@@ -44,5 +51,49 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.produtos-container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+.produtos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 30px;
+  margin: 30px;
+}
+
+.produto {
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  padding: 10px;
+  background: #fff;
+  border-radius: 4px;
+  transition: all 0.2s;
+}
+
+.produto:hover {
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+  transform: scale(1.1);
+  position: relative;
+  z-index: 1;
+}
+
+.produto img {
+  border-radius: 4px;
+  margin-bottom: 20px;
+}
+
+.titulo {
+  margin-bottom: 10px;
+}
+
+.preco {
+  color: #e80;
+  font-weight: bold;
+}
+
+.sem-resultados {
+  text-align: center;
+}
 </style>
